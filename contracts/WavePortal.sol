53d1b45fa7525b7f3bf1c3ab2353b5b4a50fdb86
Hrithik Mohan <mohanz.nebarc@gmail.com>
2022-09-24 22:03:58 +0530

    Random users will get gifts

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,11 +5,13 @@ import "hardhat/console.sol";
 
 contract WavePortal{
     uint totalWaves;
-
+    uint256 private seed;
+    mapping(address => uint256) public lastWavedAt;
     struct Wave{
         address waver;
         string data;
         uint timestamp;
+
 
     } 
     Wave[] waves;
@@ -18,21 +20,33 @@ contract WavePortal{
 
     constructor() payable {
         console.log("Im born at last!!, Awesomeness on the way...");
+	seed = (block.timestamp + block.difficulty)%100;
     }
 
+
     function wave(string memory data ) public{
+	require(
+		lastWavedAt[msg.sender] + 15 minutes < block.timestamp,
+		"Wait 15 minutes"
+	);
+	lastWavedAt[msg.sender] = block.timestamp;
         totalWaves += 1;
         console.log(msg.sender, " has just waves **Sigh**, says : ", data);
         waves.push(Wave(msg.sender,data,block.timestamp));
         emit newWave(msg.sender, data, block.timestamp);
 	uint256 prize = 0.0001 ether;
+	seed = (block.timestamp + block.difficulty+seed)%100;
+	if(seed < 34){
+		console.log("Waver won the prize");
 	require(
 		prize <= address(this).balance,
 		"Insufficient balance in contract.. Add funds"
 	);
 	(bool success, ) = (msg.sender).call{value : prize}("Thanks for waving");
 	require(success, "Failed to send money in respone to waving");
-    }
+	}
+	}
+
 
     function getAllWaves() public view returns (Wave[] memory){
         return  waves;
